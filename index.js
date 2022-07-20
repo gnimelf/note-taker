9dcd58c44e9c1aad7bce854f4cb0712f3d4ad52f
justin <fezzygo@gmail.com>
2022-07-19 23:38:54 -0400

    adds existing notes listed to notes page(server index.js:5,6,19)

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 const path = require('path');
+const db = require('./db/db.json');
+app.use(express.json());
 app.use(express.static('public'));
 
 
@@ -13,6 +15,8 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
+
+app.get('/api/notes', (req, res) => res.json(db));
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT} http://localhost:${PORT}/`)
